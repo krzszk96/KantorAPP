fa3835d359d1c531445d2a557ceef0d53bb6d5fc
krzysztof <krzszk96@gmail.com>
2020-05-01 17:18:13 +0200

    user balance update to database/ charge acc

--- app/src/main/java/com/example/kantorapp/UserAccount.java
+++ app/src/main/java/com/example/kantorapp/UserAccount.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -21,7 +22,7 @@ import com.google.firebase.database.ValueEventListener;
 
 public class UserAccount extends AppCompatActivity {
 
-    TextView userEmail, userBalance;
+    TextView userEmail, userBalancePLN, userBalanceEUR, userBalanceDOL, userBalanceGBP;
     EditText chargeAccInput;
     Button chargeAccBtn;
 
@@ -36,36 +37,43 @@ public class UserAccount extends AppCompatActivity {
         setContentView(R.layout.activity_user_account);
 
         userEmail = (TextView) findViewById(R.id.loggedEmail);
-        userBalance = (TextView) findViewById(R.id.balanceView);
+        userBalancePLN = (TextView) findViewById(R.id.balanceViewPLN);
+        userBalanceEUR = (TextView) findViewById(R.id.balanceViewEUR);
+        userBalanceDOL = (TextView) findViewById(R.id.balanceViewDOL);
+        userBalanceGBP = (TextView) findViewById(R.id.balanceViewGBP);
         chargeAccInput = (EditText) findViewById(R.id.chargeInput);
         chargeAccBtn = (Button) findViewById(R.id.chargeBtn);
 
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseUser = firebaseAuth.getCurrentUser();
 
+        userBalancePLN.setText("");
+        userBalanceEUR.setText("");
+        userBalanceDOL.setText("");
+        userBalanceGBP.setText("");
+
         userEmail.setText(firebaseUser.getEmail());
         userDataAccount();
 
-    }
-    static String encodeUserEmail(String userEmail) {
-        return userEmail.replace(".", ",");
-    }
-    static String decodeUserEmail(String userEmail) {
-        return userEmail.replace(",", ".");
+        chargeAccBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                topUpAcc();
+            }
+        });
+
     }
     private void userDataAccount() {
 
-        //final String userEmailDec = firebaseUser.getEmail();
-        //final String userEmailCod = encodeUserEmail(userEmailDec);
         String id = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
         reference = FirebaseDatabase.getInstance().getReference("users").child(id);
-                reference.addValueEventListener(new ValueEventListener() {
+        reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 Users user = dataSnapshot.getValue(Users.class);
                 String curBalance = Double.toString(user.getAccbalance());
-                userBalance.setText(curBalance);
+                userBalancePLN.setText(curBalance);
             }
 
             @Override
@@ -73,6 +81,28 @@ public class UserAccount extends AppCompatActivity {
 
             }
         });
+    }
+    private void topUpAcc(){
+        String id = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        final double UpdateAcc = Double.parseDouble(chargeAccInput.getText().toString());
+
+        reference = FirebaseDatabase.getInstance().getReference("users").child(id);
+        reference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Users user = dataSnapshot.getValue(Users.class);
+                double curBalance = user.getAccbalance();
+                curBalance = curBalance + UpdateAcc;
+                reference.child("accbalance").setValue(curBalance);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
 
     }
 }
